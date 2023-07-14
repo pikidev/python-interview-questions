4223898a0a8b2b3b330e1153bd9d308392c5c815
Pallavi Mishra <emailtopallavimishra>
2023-07-14 08:27:27 +0530

    01_list

---
+++ 01_list/multiply/read.py
@@ -0,0 +1,10 @@
+Given a list, print the value obtained after multiplying all numbers in a list. 
+
+
+
+Input :  list1 = [1, 2, 3] 
+Output : 6 
+Explanation: 1*2*3=6 
+
+Input : list1 = [3, 2, 4] 
+Output : 24 
